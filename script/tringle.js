7e02922c3f7b7d271c7f96f9a72cb3a94eea870d
Bakir Bhuyain <hemalbakir@gmail.com>
2025-08-06 15:53:02 +0600

    Initial commit

--- script/tringle.js
+++ script/tringle.js
@@ -1,3 +1,23 @@
+/**
+ * Objective : Gat base, height, of a triangle . calculate the area by using the provided 
+   formula and then display the area
+   * step 1 : get base value  
+   * step 2 : added an id in the base input field 
+   * step 3 : use get elementById to access the input field
+   * step 4 : get value from the input field 
+   * step 5 : convert the value to number by using perseFloat.
+ */
+
 function calculateTriangleArea (){
-      console.log('inside the Function')
+  const triangleBaseInput = document.getElementById('triangle-base')
+  const triangleBaseText = triangleBaseInput.value;
+  const base = parseFloat(triangleBaseText);
+  console.log(base);
 }
+
+      //get triangle height
+      // const triangleHeightInput = document.getElementById('triangle-height');
+      // const triangleBaseHeight = triangleHeightInput.value;
+      // const triangleHeight = parseFloat(triangleBaseHeight); 
+      // console.log(triangleHeight);
+
